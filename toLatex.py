a1953b30e0872b6cb2e547148cb1d3dde7ebb83f
Lux Miranda <19409711+LuxMiranda@users.noreply.github.com>
2022-04-05 17:25:18 -0400

    Pipeline choo choo!

--- toLatex.py
+++ toLatex.py
@@ -1,16 +1,47 @@
 import yaml
 import re
 
+CV_YAML      = 'cv.yaml'
+TEMPLATE_TEX = 'tex/template.tex'
+MAIN_TEX     = 'tex/main.tex'
 
-cv = yaml.safe_load(open('cv.yaml', 'r'))
-texFile = open('tex/template.tex','r')
-tex = texFile.read()
-texFile.close()
+RENDER = {
+        'en' : True,
+        'sv' : True,
+        'no' : True
+}
 
-tex = re.sub('\$FIRSTNAME',cv['first name'],tex)
-tex = re.sub('\$LASTNAME',cv['last name'],tex)
+def load():
+    with open(CV_YAML, 'r') as f:
+        CV = yaml.safe_load(f)
+    with open(TEMPLATE_TEX,'r') as f:
+        TEX = f.read()
+    return CV, TEX
 
-texFile = open('tex/main.tex','w')
-texFile.write(tex)
-texFile.close()
+def sub(var, tex, cv):
+    templateVar = '\${}'.format(var.upper())
+    return re.sub(templateVar, cv[var], tex)
 
+def langSub(var, tex, cv):
+    for lang in cv['languages']:
+        lvar = '\${}-{}'.format(var.upper(), lang.upper())
+        lsub = '({})'.format(cv['{}-{}'.format(var,lang)]) if RENDER[lang] else ''
+        tex = re.sub(lvar, lsub, tex)
+    return tex
+
+
+def write(tex):
+    texFile = open(MAIN_TEX,'w')
+    texFile.write(tex)
+    texFile.close()
+
+def main():
+    cv, tex = load()
+    tex = sub('firstname', tex, cv)
+    tex = sub('lastname', tex, cv)
+    tex = langSub('pronouns', tex, cv)
+    write(tex)
+
+
+if __name__ == '__main__':
+    main()
